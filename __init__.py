a0d1aa70b296c2d8b5e4afa2f4757de4e3ff9405
AntoineRigaut <rigautantoine@hotmail.com>
2018-11-16 22:15:28 -0700

    Added some AFT classes

--- __init__.py
+++ __init__.py
@@ -4,4 +4,4 @@ from .aalenadditive import AalenAdditive
 from .coxph import CoxPH
 from .exponential import ExponentialUnivariate
 from .loglogistic import LogLogisticUnivariate
-from .weibull import WeibullUnivariate
+from .weibull import WeibullUnivariate, Weibull
